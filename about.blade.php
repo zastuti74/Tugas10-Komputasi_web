8c386b85eef127504b3a2ca0a71b205d025a98bb
zastuti74 <zastuti74@gmail.com>
2024-12-09 15:09:47 +0700

    Create about.blade.php

---
+++ about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>INI ABOUT</h1>
+    <nav>
+        <a href="{{ route('home') }}">Home</a>
+        <a href="{{ route('about') }}">About</a>
+        <a href="{{ route('contact') }}">Contact</a>
+    </nav>
+</body>
+</html>
